8b7dc6e8a083c4e974fdc952fb1cc02bb1e1cbdc
Crina Tudorachi <crina.tudorachi@3pillarglobal.com>
2018-12-13 12:27:26 +0200

    1st commit

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,14 +14,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-
-
-
-        
-
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "This is my action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
     }
